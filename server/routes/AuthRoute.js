2291d715cf23239d2f70da93d61d40fa3a17675a
Ahsan Javed <ahsannjavaid@gmail.com>
2024-02-10 17:34:43 +0500

    getUsers API protect

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/get-users", getUsers);
+router.get("/get-users", requireSignIn, getUsers);
 router.get("/get-single-user/:id", getSingleUser);
 router.delete("/delete-single-user/:id", deleteSingleUser);
 router.delete("/delete-users", deleteUsers);
